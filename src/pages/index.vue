fb842d78ff854d1f33153c51434f1e5cf62666cf
syanodes <syaifullah12310@gmail.com>
2024-01-19 23:05:08 +0700

    edit and add chain

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -17,8 +17,9 @@ const chains = computed(() => {
     const lowercaseKeywords = keywords.value.toLowerCase();
 
     return Object.values(dashboard.chains).filter(
-      (x: ChainConfig) => x.chainName.toLowerCase().indexOf(lowercaseKeywords) > -1 
-      || x.prettyName.toLowerCase().indexOf(lowercaseKeywords) > -1
+      (x: ChainConfig) =>
+        x.chainName.toLowerCase().indexOf(lowercaseKeywords) > -1 ||
+        x.prettyName.toLowerCase().indexOf(lowercaseKeywords) > -1
     );
   } else {
     return Object.values(dashboard.chains);
@@ -26,18 +27,29 @@ const chains = computed(() => {
 });
 
 const featured = computed(() => {
-  const names = ["cosmos", "osmosis", "akash", "celestia", "evmos", "injective", "dydx", "noble"];
+  const names = [
+    'cosmos',
+    'osmosis',
+    'akash',
+    'celestia',
+    'evmos',
+    'injective',
+    'dydx',
+    'noble',
+  ];
   return chains.value
-    .filter(x => names.includes(x.chainName))
-    .sort((a, b)=> (names.indexOf(a.chainName) - names.indexOf(b.chainName)))
-})
+    .filter((x) => names.includes(x.chainName))
+    .sort((a, b) => names.indexOf(a.chainName) - names.indexOf(b.chainName));
+});
 
-const chainStore = useBlockchain()
+const chainStore = useBlockchain();
 </script>
 <template>
   <div class="">
-    <div class="flex md:!flex-row flex-col items-center justify-center mb-6 mt-14 gap-2">
-      <div class="w-16 rounded-full">
+    <div
+      class="flex md:!flex-row flex-col items-center justify-center mb-6 mt-14 gap-2"
+    >
+      <!-- <div class="w-16 rounded-full">
         <svg version="1.0" xmlns="http://www.w3.org/2000/svg" 
           viewBox="0 0 150.000000 132.000000"
           preserveAspectRatio="xMidYMid meet">
@@ -57,16 +69,16 @@ const chainStore = useBlockchain()
             -1 4 -43 78 -93 165 l-90 157 -187 0 c-104 0 -188 -2 -188 -5z"/>
           </g>
         </svg>
-      </div>
-      <h1 class="text-primary dark:invert text-3xl md:!text-6xl font-bold">
-        {{ $t('pages.title') }}
+      </div> -->
+      <h1 class="text-white text-3xl md:!text-6xl font-bold">
+        SyaNodes Explorer
       </h1>
     </div>
-    <div class="text-center text-base">
+    <!-- <div class="text-center text-base">
       <p class="mb-1">
         {{ $t('pages.slogan') }}
       </p>
-    </div>
+    </div> -->
     <div
       v-if="dashboard.status !== LoadingStatus.Loaded"
       class="flex justify-center"
@@ -74,14 +86,18 @@ const chainStore = useBlockchain()
       <progress class="progress progress-info w-80 h-1"></progress>
     </div>
 
-    <div v-if="featured.length>0" class="text-center text-base mt-6 text-primary">
-      <h2 class="mb-6"> Featured Blockchains 🔥 </h2>
+    <div
+      v-if="featured.length > 0"
+      class="text-center text-base mt-6 text-primary"
+    >
+      <h2 class="mb-6">Featured Blockchains 🔥</h2>
     </div>
 
-    <div v-if="featured.length>0"
+    <div
+      v-if="featured.length > 0"
       class="grid grid-cols-1 gap-4 mt-6 md:!grid-cols-3 lg:!grid-cols-4 2xl:!grid-cols-5"
     >
-    <ChainSummary
+      <ChainSummary
         v-for="(chain, index) in featured"
         :key="index"
         :name="chain.chainName"
@@ -92,10 +108,18 @@ const chainStore = useBlockchain()
       <h2 class="mb-6">{{ $t('pages.description') }}</h2>
     </div>
 
-    <div class="flex items-center rounded-lg bg-base-100  border border-gray-200 dark:border-gray-700 mt-10">
-      <Icon icon="mdi:magnify" class="text-2xl text-gray-400 ml-3"/>
-      <input :placeholder="$t('pages.search_placeholder')" class="px-4 h-10 bg-transparent flex-1 outline-none text-base" v-model="keywords" />
-      <div class="px-4 text-base hidden md:!block">{{ chains.length }}/{{ dashboard.length }}</div>
+    <div
+      class="flex items-center rounded-lg bg-base-100 border border-gray-200 dark:border-gray-700 mt-10"
+    >
+      <Icon icon="mdi:magnify" class="text-2xl text-gray-400 ml-3" />
+      <input
+        :placeholder="$t('pages.search_placeholder')"
+        class="px-4 h-10 bg-transparent flex-1 outline-none text-base"
+        v-model="keywords"
+      />
+      <div class="px-4 text-base hidden md:!block">
+        {{ chains.length }}/{{ dashboard.length }}
+      </div>
     </div>
 
     <div
@@ -111,7 +135,7 @@ const chainStore = useBlockchain()
 </template>
 
 <style scoped>
- .logo path{
+.logo path {
   fill: #171d30;
 }
 </style>
